8b28c4723087127c73dd0d609bfe9fe411e7131d
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2024-10-15 16:18:09 +0100

    first commit

---
+++ src/Component/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='bg-indigo-700 py-20 mb-4'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+            <div className="text-center">
+                <h1 className='text-4xl font-extrabold text-white sm:5xl md:text-6xl '>Become a React Dev Today!</h1>
+                <p className='my-4 text-xl text-white'>Find the React Job that dits your skill set.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
